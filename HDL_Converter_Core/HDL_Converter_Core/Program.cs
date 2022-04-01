461d2e156d1332e272822c41c96cc47b561a7a07
m47812 <49725858+m47812@users.noreply.github.com>
2022-04-01 15:04:14 +0200

    Created Core Project

---
+++ HDL_Converter_Core/HDL_Converter_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HDL_Converter_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
