48605594ee4b72f7059ae0c560ced5b575b4e388
Robin Mueller <m47812@gmail.com>
2022-04-12 15:45:32 +0200

    Initialize from HDL

--- HDL_Converter_App/HDL_Converter_Classes/HDL_Structures/VHDLConstructs.cs
+++ HDL_Converter_App/HDL_Converter_Classes/HDL_Structures/VHDLConstructs.cs
@@ -21,7 +21,28 @@ namespace HDL_Converter_Classes.HDL_Structures
 
         protected override void initializeFormHDLCode(string hdlCode)
         {
-            throw new NotImplementedException();
+            //Find entity boundries
+            string entity_L = hdlCode.ToLower();
+            int startIndex = entity_L.IndexOf("entity") + 6;
+            if (startIndex == -1) throw new FormatException("Beginning of entity not found");
+            int endIndex = entity_L.IndexOf("end entity", startIndex);
+            if (endIndex == -1) throw new FormatException("Entity end not found (only one whitspace between \"entity\" and \"end\" tollerated)");
+            string entity = hdlCode.Substring(startIndex, endIndex - startIndex);
+            entity_L = entity_L.Substring(startIndex, endIndex - startIndex);
+            //Extract Generics (if given)
+            startIndex = entity_L.IndexOf("generic");
+            if(startIndex != -1)
+            {
+                Tuple<int, int> genericBoundries = this.getTopParenteses('(', ')', entity_L.Substring(startIndex));
+                this.initializeParameters(entity.Substring(genericBoundries.Item1 + 1, genericBoundries.Item1 - genericBoundries.Item2));
+            }
+            //Extract Port signals
+            startIndex = entity_L.IndexOf("port");
+            if(startIndex != -1)
+            {
+                Tuple<int, int> portBoundries = this.getTopParenteses('(', ')', entity_L.Substring(startIndex));
+                this.initializeWires(entity.Substring(portBoundries.Item1 + 1, portBoundries.Item1 - portBoundries.Item2));
+            }           
         }
 
         protected override void initializeParameters(string hdlCode)
