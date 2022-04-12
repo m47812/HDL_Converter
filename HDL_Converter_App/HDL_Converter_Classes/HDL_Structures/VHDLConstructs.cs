ffc32cabad813dfa8d1ea776a3dba6e7b3a38167
Robin Mueller <m47812@gmail.com>
2022-04-12 14:59:21 +0200

    VHDL Support base structure

---
+++ HDL_Converter_App/HDL_Converter_Classes/HDL_Structures/VHDLConstructs.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HDL_Converter_Classes.HDL_Structures
+{
+    /// <summary>
+    /// A Module in VHDL (Entity) containing IO signals and generics
+    /// </summary>
+    class VHDLModule : HDLModule
+    {
+        public override string generateModuleInstantiation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string generateWireDeclaration()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void initializeFormHDLCode(string hdlCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void initializeParameters(string hdlCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void initializeWires(string hdlCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// A Wire in the VHDL (signal in VHDL) domain serving as an IO to a module
+    /// </summary>
+    class VHDLWire : Wire
+    {
+        public override string buildComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string generateInstantiationLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string generateWireDeclarationLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void initializeFromCodeLine(string codeLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// A Parameter in the VHDL domain (generic in VHDL) serving as a configuration parameter for the module
+    /// </summary>
+    class VHDLParameter : Parameter
+    {
+        public override string buildComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string generateInstantiationLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string generateWireDeclarationLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void initializeFromCodeLine(string codeLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
