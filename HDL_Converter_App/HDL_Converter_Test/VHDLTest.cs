80997310ac744077915052210f46db825efd873b
Robin Mueller <m47812@gmail.com>
2022-04-12 15:58:33 +0200

    Created VHDL Test evironment

---
+++ HDL_Converter_App/HDL_Converter_Test/VHDLTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HDL_Converter_Classes.HDL_Structures;
+using System.Collections.Generic;
+
+namespace HDL_Converter_Test
+{
+    [TestClass]
+    class VHDLTest
+    {
+
+    }
+}
