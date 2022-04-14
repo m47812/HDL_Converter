318ff94411f5b006ce7fc362740efdc38513c4fd
Robin Mueller <m47812@gmail.com>
2022-04-14 19:51:15 +0200

    Testbench GUI support

--- HDL_Converter_App/HDL_Converter_GUI/GUI_Components/TestbenchControl.xaml.cs
+++ HDL_Converter_App/HDL_Converter_GUI/GUI_Components/TestbenchControl.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace HDL_Converter_GUI.GUI_Components
 {
@@ -20,9 +20,30 @@ namespace HDL_Converter_GUI.GUI_Components
     /// </summary>
     public partial class TestbenchControl : UserControl
     {
+        private string target_path;
         public TestbenchControl()
         {
             InitializeComponent();
+        }
+
+        private void click_folder_location(object sender, EventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.Description = "Select folder to store testbench in";
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                if(result == System.Windows.Forms.DialogResult.OK)
+                {
+                    this.target_path = dialog.SelectedPath;
+                    this.laFolderLocation.Content = "Path: " + this.target_path;
+                    btGenerateTestbench.IsEnabled = true;
+                }
+            }
+        }
+
+        private void click_generate_testbench(object sender, EventArgs e)
+        {
+
         }
     }
 }
